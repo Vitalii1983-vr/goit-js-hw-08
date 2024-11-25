5086a5e5b02febf3c6139b27a05a4ed3701b4e10
Vitalii Rudenko <vradokat1@gmail.com>
2024-11-25 14:46:51 +0200

    hw8 JS finish

---
+++ js/gallery.js
@@ -0,0 +1,142 @@
+const images = [
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+      description: "Hokkaido Flower",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+      description: "Container Haulage Freight",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+      description: "Aerial Beach View",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+      description: "Flower Blooms",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+      description: "Alpine Mountains",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+      description: "Mountain Lake Sailing",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+      description: "Alpine Spring Meadows",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+      description: "Nature Landscape",
+    },
+    {
+      preview:
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+      original:
+        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+      description: "Lighthouse Coast Sea",
+    },
+  ];
+  
+  //build gallery from images
+  const galleryItems = images
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+  
+  const galleryList = document.querySelector(".gallery");
+  galleryList.innerHTML = galleryItems;
+  
+  //Listeners
+  
+  galleryList.addEventListener("click", onGalleryItemClick);
+  
+  function onGalleryItemClick(event) {
+    event.preventDefault();
+  
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+    const largeImageURL = event.target.dataset.source;
+  
+    const largeImageModal = basicLightbox.create(
+      `
+      <div class="modal-image">
+        <img class="large-image" src="${largeImageURL}">
+      </div>
+      `
+    );
+  
+    largeImageModal.show();
+  
+    // const leftArrow = document.querySelector(".left-arrow");
+    // const rightArrow = document.querySelector(".right-arrow");
+    // leftArrow.addEventListener("click", showPreviousImage);
+    // rightArrow.addEventListener("click", showNextImage);
+  
+    document.addEventListener("keydown", onKeyPress);
+  }
+  
+  function onKeyPress(event) {
+    if (event.key === "ArrowLeft") {
+      showPreviousImage();
+    } else if (event.key === "ArrowRight") {
+      showNextImage();
+    }
+  }
+  
+  let currentIndex = 0;
+  const image = document.querySelector(".modal-image img");
+  
+  function showPreviousImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateModalImage();
+  }
+  
+  function showNextImage() {
+    currentIndex = (currentIndex + 1 + images.length) % images.length;
+    updateModalImage();
+  }
+  
+  function updateModalImage() {
+    image.src = images[currentIndex].original;
+    image.alt = images[currentIndex].description;
+  }
